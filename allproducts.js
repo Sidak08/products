f904754c00a1d1bbe287fe04101241f4b58dca30
Sidak08 <magicalsidak@gmail.com>
2023-04-20 13:40:02 -0400

    First Commit

---
+++ allproducts.js
@@ -0,0 +1,168 @@
+
+let Womensproducts = [
+  {
+    imageSrc:
+      "https://assets.zyrosite.com/YZ9MxPwN40Hxl037/screen-shot-2023-04-17-at-7.00.14-pm-mP4ZEKOgl2CV3bWn.png",
+    title: "Womens Gift Set",
+    description: "",
+    price: "CA$15.99",
+  },
+  {
+    imageSrc:
+      "https://assets.zyrosite.com/YZ9MxPwN40Hxl037/screen-shot-2023-04-17-at-7.04.05-pm-YlenpVbM4ehDnMrB.png",
+    title: "Megnetic Couple Necklace",
+    description: "",
+    price: "CA$9.99",
+  },
+  {
+    imageSrc:
+      "https://assets.zyrosite.com/YZ9MxPwN40Hxl037/screen-shot-2023-04-17-at-7.09.02-pm-AGBqeKg9xjfE5axk.png",
+    title: "Mama Braclet",
+    description: "",
+    price: "CA$7.99",
+  },
+  {
+    imageSrc:
+      "https://assets.zyrosite.com/YZ9MxPwN40Hxl037/screen-shot-2023-03-22-at-5.07.09-pm-YlenW0zRR5h5V4B6.png",
+    title: "Womans Earrings",
+    description: "",
+    price: "CA$14.99",
+  },
+  {
+    imageSrc:
+      "https://assets.zyrosite.com/YZ9MxPwN40Hxl037/screen-shot-2023-03-25-at-11.52.03-pm-ALp8PaEqV0s5GVv9.png",
+    title: "Womans sterling silver",
+    description: "",
+    price: "CA$59.99",
+  },
+  {
+    imageSrc:
+      "https://assets.zyrosite.com/YZ9MxPwN40Hxl037/screen-shot-2023-03-25-at-11.52.03-pm-ALp8PaEqV0s5GVv9.png",
+    title: "Womans Sterling Silver Ring",
+    description: "",
+    price: "CA$14.99",
+  },
+  {
+    imageSrc:
+      "https://assets.zyrosite.com/YZ9MxPwN40Hxl037/screen-shot-2023-04-17-at-7.08.03-pm-mP4ZEKrDGwsD5Kq0.png",
+    title: "Arabic Love Ring",
+    description: "",
+    price: "CA$9.99",
+  },
+];
+
+let mensproducts = [
+  {
+    imageSrc:
+      "https://assets.zyrosite.com/YZ9MxPwN40Hxl037/screen-shot-2023-04-14-at-11.17.30-pm-AE0EKEa9J1u9XK5p.png",
+    title: "4k professional drone",
+    description: "",
+    price: "CA$59.99",
+  },
+  {
+    imageSrc:
+      "https://assets.zyrosite.com/YZ9MxPwN40Hxl037/screen-shot-2023-04-14-at-11.30.57-pm-mv0LVLzgw3Fn5Dvj.png",
+    title: "Flying UFO",
+    description: "",
+    price: "CA$29.99",
+  },
+  {
+    imageSrc:
+      "https://assets.zyrosite.com/YZ9MxPwN40Hxl037/screen-shot-2023-04-14-at-11.36.17-pm-AwvOoOoE49tlX7r5.png",
+    title: "Led Sound Bar",
+    description: "",
+    price: "CA$19.99",
+  },
+  {
+    imageSrc:
+      "https://assets.zyrosite.com/YZ9MxPwN40Hxl037/screen-shot-2023-04-15-at-12.43.33-am-Yan7V2kEyPFE9gRq.png",
+    title: "Coustom Engraved Pen",
+    description: "",
+    price: "CA$9.99",
+  },
+  {
+    imageSrc:
+      "https://assets.zyrosite.com/YZ9MxPwN40Hxl037/screen-shot-2023-03-25-at-11.51.57-pm-YZ9MjJyG9ZiKGkXV.png",
+    title: "Luxery mens ring",
+    description: "",
+    price: "CA$14.99",
+  },
+  {
+    imageSrc:
+      "https://assets.zyrosite.com/YZ9MxPwN40Hxl037/screen-shot-2023-04-14-at-11.26.03-pm-A0x3p3GnMnFjBGKQ.png",
+    title: "Fidget Pen",
+    description: "",
+    price: "CA$24.99",
+  },
+  {
+    imageSrc:
+      "https://assets.zyrosite.com/YZ9MxPwN40Hxl037/screen-shot-2023-03-25-at-11.51.19-pm-dWxnONX4PlfeDKXq.png",
+    title: "Classic quartz mens",
+    description: "",
+    price: "CA$42.99",
+  },
+  {
+    imageSrc:
+      "https://assets.zyrosite.com/YZ9MxPwN40Hxl037/screen-shot-2023-04-15-at-12.48.28-am-mv0LVxRQ8btE0Kgk.png",
+    title: "Minimalist Leather Wallet",
+    description: "",
+    price: "CA$39.99",
+  }
+]
+
+let bestsellar = [
+  {
+    imageSrc: "https://assets.zyrosite.com/YZ9MxPwN40Hxl037/screen-shot-2023-04-17-at-7.00.14-pm-mP4ZEKOgl2CV3bWn.png",
+    title: "Womens Gift Set",
+    description: "",
+    price: "CA$15.99",
+  },
+  {
+    imageSrc: "https://assets.zyrosite.com/YZ9MxPwN40Hxl037/screen-shot-2023-03-22-at-5.07.09-pm-YlenW0zRR5h5V4B6.png",
+    title: "Womens earings",
+    description: "",
+    price: "CA$14.99",
+  },
+  {
+    imageSrc: "https://assets.zyrosite.com/YZ9MxPwN40Hxl037/screen-shot-2023-04-14-at-11.17.30-pm-AE0EKEa9J1u9XK5p.png",
+    title: "4k professional drone",
+    description: "",
+    price: "CA$59.99",
+  },
+  {
+    imageSrc: "https://assets.zyrosite.com/YZ9MxPwN40Hxl037/screen-shot-2023-04-14-at-11.30.57-pm-mv0LVLzgw3Fn5Dvj.png",
+    title: "Flying UFO",
+    description: "",
+    price: "CA$29.99",
+  },
+  {
+    imageSrc: "https://assets.zyrosite.com/YZ9MxPwN40Hxl037/screen-shot-2023-04-14-at-11.36.17-pm-AwvOoOoE49tlX7r5.png",
+    title: "Led Sound Bar",
+    description: "",
+    price: "CA$19.99",
+  },
+  {
+    imageSrc: "https://assets.zyrosite.com/YZ9MxPwN40Hxl037/screen-shot-2023-03-25-at-11.52.03-pm-ALp8PaEqV0s5GVv9.png",
+    title: "Womans sterling silver",
+    description: "",
+    price: "CA$59.99",
+  },
+  {
+    imageSrc: "https://assets.zyrosite.com/YZ9MxPwN40Hxl037/screen-shot-2023-03-25-at-11.51.57-pm-YZ9MjJyG9ZiKGkXV.png",
+    title: "Luxery mens ring",
+    description: "",
+    price: "CA$14.99",
+  },
+  {
+    imageSrc: "https://assets.zyrosite.com/YZ9MxPwN40Hxl037/screen-shot-2023-04-14-at-11.26.03-pm-A0x3p3GnMnFjBGKQ.png",
+    title: "Fidget Pen",
+    description: "",
+    price: "CA$24.99",
+  },
+  {
+    imageSrc: "https://assets.zyrosite.com/YZ9MxPwN40Hxl037/screen-shot-2023-03-25-at-11.51.19-pm-dWxnONX4PlfeDKXq.png",
+    title: "Classic quartz mens",
+    description: "",
+    price: "CA$42.99",
+  }
+]
